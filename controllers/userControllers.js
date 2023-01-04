3755a1a775fbed914f066de93de0a70264b6a6af
Leon Christian Habineza <leonchristianhabineza@Leons-MacBook-Pro.local>
2023-01-04 13:39:57 -0600

    Registers a new user
    A new user can create an account and all fields are validated.

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,20 @@
-// import User from '../models/userModel.js';
+import User from '../models/userModel.js';
+import {databaseErrorHandlingFunction} from '../helpers/userHelpers.js';
 
 
  export const signUp = async(req, res) =>{
-        res.status(200).json({message: 'User will be created'})
+        const {username, email, password} = req.body;
+       try{
+        const user = await User.create({
+            username: username.replace(/\s/g, '').trim(),
+            email,
+            password,
+        });
+
+        res.status(200).json({user});
+       }catch(error) {
+       
+        const errors = databaseErrorHandlingFunction(error);
+        return res.status(400).json(errors);
+       }
     }
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import {isEmail} from 'validator';
+import validator from 'validator';
 import bcrypt from 'bcrypt';
 
 
@@ -16,14 +16,14 @@ const userSchama = new mongoose.Schema({
         type: String,
         unique: true,
         required : [true, 'Email is required'],
-        validate : [isEmail, 'Please enter a valid email'],
+        validate : [validator.isEmail, 'Please enter a valid email'],
         lowercase: true
     },
     password: {
         type: String,
         unique: true,
-        min : [3, 'Minimum passowrd length is 6 characters'],
         required : [true, 'Password is required'],
+        validate: [validator.isStrongPassword, 'The password should be 8 characters long, with at least 1 small and capital letters, and 1 symbol.']
     },
     profilePicture : {
         type: String,
@@ -41,10 +41,14 @@ const userSchama = new mongoose.Schema({
         type: Array,
         default: []
     },
-
+    isAdim: {
+        type: Boolean,
+        default: false
+    }
 
 },
 {timestamps: true});
+
 
 userSchama.pre('save', async function (next) {
     const salt = await bcrypt.genSalt(10);
